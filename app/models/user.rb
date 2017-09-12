b06da442a70c310dd8c9f2931b9af041b1b8e994
nadinesk <nadine.khattak@gmail.com>
2017-09-12 16:12:55 -0400

    user reference on books and relationships on models for user and books'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+	has_many :books
+	
+	has_secure_password
+
+	validates :email, presence: true	
+	validates :email, uniqueness: true
+	validates :password, length: {minimum: 8}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906161518) do
+ActiveRecord::Schema.define(version: 20170912200936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20170906161518) do
     t.string "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "books", "users"
 end
